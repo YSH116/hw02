8a3a15415240e9d34880bc8daa0db453bbab4483
YSH116 <74852896+YSH116@users.noreply.github.com>
2021-03-31 18:47:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,43 +3,52 @@
 
 AnalogOut aout(PA_4);
 AnalogIn ain(D11);
-DigitalIn down(D6);
-DigitalIn sel(D8);
-DigitalIn up(D9);
+DigitalIn down(D5);
+DigitalIn sel(D9);
+DigitalIn up(D3);
 uLCD_4DGL uLCD(D1, D0, D2);
-int freq = 0, j, ok; //T = 2000;
-float ADCdata[100];
 
 int main() 
 {   
-    freq = 320; //cut-off freq 1/(2*3.14*(3900+3900)*47*10^-9) = 434.358hz, T = 2.302ms
+    int freq = 50, ratio = 1, i = 1, l = 1, sample_pt = 1, T = 2000, sample = 50;//cut-off freq 1/(2*3.14*(36800+3900)*47*10^-9) = 83.243hz, T = 0.012ms
+    float ADCdata[500];
     uLCD.text_width(2); 
     uLCD.text_height(2);
     uLCD.color(RED);
     uLCD.locate(0,2);
     uLCD.printf("%3dHz", freq);
     while (1) {
-        if (sel) ok = 1;
         if (up) {
-            freq += 20;
+            freq += 10;
             uLCD.locate(0,2);
             uLCD.printf("%3dHz", freq);
         }
-        else if (down) {
-            freq -= 20;
+        if (down) {
+            freq -= 10;
             uLCD.locate(0,2);
             uLCD.printf("%3dHz", freq);
         }
-        else if (ok) { // generate triangular wave & S = 2
-            if (j == 100) j = 0; 
-            else if (j <= 20) aout = j/20.0/1.1;                
-            else aout = (100-j)/80.0/1.1;
-            //T = 1/freq*1000000;
-            ADCdata[j++] = ain;
-            //wait_us((T-1260)/100);
+        if (sel) { 
+            T = 1000000/freq;
+            sample = 100*freq/500;
+            uLCD.locate(0,2);
+            uLCD.printf("%3dHz", freq);
+            for (l = 0; l < 500; l++)
+                printf("%f\r\n", ADCdata[l]);
         }
-        /*if (j == 100) {
-            for (j = 0; j <= 100; j++) printf("%f\r\n", ADCdata[j]);
-        }*/
+        if (ratio == 100) ratio = 0; // generate triangular wave & S = 2 
+        if (ratio <= 20) aout = ratio/20.0/1.1;                
+        if (ratio > 20) aout = (100-ratio)/80.0/1.1;
+        ratio++;
+        if (sample_pt >= 500) { // sample 
+            sample_pt = 0; 
+            i = 0;
+        } 
+        else if (i % sample == 0) {
+            ADCdata[sample_pt] = ain;
+            sample_pt++;
+        }
+        i++;
+        wait_us(T/100-10);
     }
 }
